d5697d10eed8e774ac57c5893ac3b3987e0ba252
Alejandro Osorio <avot3110@hotmail.com>
2023-05-03 13:29:01 -0400

    modifiying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+
+document.querySelector('main#main').remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+let name = 'Alejandro';
+newHeader.innerHTML = `${name} is the champion`;
+
+document.body.appendChild(newHeader);
